9fdefc162fea7c314b5a2b26bc73e821bd88c9d2
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-05-20 19:33:46 -0500

    Removing TPC/Optical readout start time from Trigger data product --kazu

--- RawData/TriggerData.h
+++ RawData/TriggerData.h
@@ -21,9 +21,6 @@ namespace raw {
       fTriggerTime         = std::numeric_limits<double>::max();
       fBeamGateTime        = std::numeric_limits<double>::max();
       
-      fReadOutStartOptical = std::numeric_limits<double>::max();
-      fReadOutStartTPC     = std::numeric_limits<double>::max();
-      
       fTriggerBits         = 0x0;
     }
     
@@ -32,8 +29,6 @@ namespace raw {
     unsigned int fTriggerNumber;       ///< Trigger counter
     double       fTriggerTime;         ///< Trigger time w.r.t. electronics clock T0
     double       fBeamGateTime;        ///< BeamGate time w.r.t. electronics clock T0
-    double       fReadOutStartTPC;     ///< TPC readout start time w.r.t. electronics clock T0
-    double       fReadOutStartOptical; ///< Optical readout start time w.r.t. electronics clock T0
     unsigned int fTriggerBits;         ///< Trigger bits ... dedicated bit-by-bit function available
 
   public:
@@ -44,14 +39,10 @@ namespace raw {
     Trigger(unsigned int counter,
 	    double       trigger_time,
 	    double       beamgate_time,
-	    double       tpc_readout_start,
-	    double       opt_readout_start,
 	    uint32_t     bits)
       : fTriggerNumber       ( counter           ),
 	fTriggerTime         ( trigger_time      ),
 	fBeamGateTime        ( beamgate_time     ),
-	fReadOutStartTPC     ( tpc_readout_start ),
-	fReadOutStartOptical ( opt_readout_start ),
 	fTriggerBits         ( bits              )
     {}
 
@@ -61,10 +52,6 @@ namespace raw {
     double       TriggerTime  ()          const { return fTriggerTime;         }
     /// BeamGate time w.r.t. electronics clock T0 in ns
     double       BeamGateTime ()          const { return fBeamGateTime;        }
-    /// Beginning of TPC readout start time w.r.t. electronics clock T0 in ns
-    double       ReadOutStartTPC     ()   const { return fReadOutStartTPC;     }
-    /// Beginning of Optical readout start time w.r.t. electronics clock T0 in ns
-    double       ReadOutStartOptical ()   const { return fReadOutStartOptical; }
     /// Trigger Bits
     unsigned int TriggerBits  ()          const { return fTriggerBits;         }
     /// Accessor to specific bit
