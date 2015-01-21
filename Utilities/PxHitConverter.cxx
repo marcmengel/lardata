ddc3a69a8f8fd5d89de6280f1d89f63dd118cfd6
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-01-21 08:18:19 -0600

    bug fix in hit vector creation --kazu

--- Utilities/PxHitConverter.cxx
+++ Utilities/PxHitConverter.cxx
@@ -14,7 +14,7 @@ namespace util {
   
    /// Generate: from 1 set of hits => 1 set of PxHits using indexes (association)
   void PxHitConverter::GeneratePxHit(const std::vector<art::Ptr<recob::Hit>> hits,
-				std::vector<util::PxHit> &pxhits) const
+				     std::vector<util::PxHit> &pxhits) const
   {
 
     if(!(hits.size())) throw UtilException(Form("Hit list empty! (%s)",__FUNCTION__));
@@ -25,61 +25,38 @@ namespace util {
     hit_index.reserve(hits.size());
 
     //generate full index
-    for(unsigned int ix=0; ix<hits.size();++ix ) {
-      
-      hit_index.at(ix)=ix;
-      
-    }      
+    for(unsigned int ix=0; ix<hits.size();++ix ) hit_index.push_back(ix);
 
     GeneratePxHit(hit_index,hits,pxhits);
     
   }
   
-  
-  
-   void PxHitConverter::GenerateSinglePxHit(const art::Ptr<recob::Hit> hit,
-		             util::PxHit &pxhit) const
-	{
-	
-   
-  
-  
-    util::GeometryUtilities  gser;
+  void PxHitConverter::GenerateSinglePxHit(const art::Ptr<recob::Hit> hit,
+					   util::PxHit &pxhit) const
+  {
     
-     
-     
+    util::GeometryUtilities  gser;
     UChar_t plane = hit->WireID().Plane;
-	 
+    
     pxhit.t = hit->PeakTime() * gser.TimeToCm();
     pxhit.w = hit->WireID().Wire     * gser.WireToCm();
-
+    
     pxhit.charge = hit->Charge();
     pxhit.peak   = hit->Charge(true);
     pxhit.plane  = plane;  
-	  
-	  
-			       
-	}
-		       
-		       
-		       
-  
-  
-  
-  
-  
+    
+  }
     
   /// Generate: from 1 set of hits => 1 set of PxHits using indexes (association)
   void PxHitConverter::GeneratePxHit(const std::vector<unsigned int>& hit_index,
-				const std::vector<art::Ptr<recob::Hit>> hits,
-				std::vector<util::PxHit> &pxhits) const
+				     const std::vector<art::Ptr<recob::Hit>> hits,
+				     std::vector<util::PxHit> &pxhits) const
   {
 
     if(!(hit_index.size())) throw UtilException(Form("Hit list empty! (%s)",__FUNCTION__));
     
     pxhits.clear();
     pxhits.reserve(hit_index.size());
-
 
     for(auto const& index : hit_index) {
       
@@ -90,9 +67,7 @@ namespace util {
 
       pxhits.push_back(h);
     }      
-
+    
   }
-  
-  
   
 }// end namespace util
