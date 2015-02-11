f4067bc6b51c2de6d14908ac229016c58c64639e
Brian_Rebel <brebel@fnal.gov>
2015-02-10 22:59:36 -0600

    add TimeStamp information to AuxDetDigit.  uint64_t, upper 32 bits used for seconds since 1970, lower for nanosecond precision

--- RawData/AuxDetDigit.h
+++ RawData/AuxDetDigit.h
@@ -27,13 +27,17 @@ namespace raw {
     std::vector<short> fADC;        ///< vector of adc counts
     unsigned short     fChannel;    ///< channel in the readout
     std::string        fAuxDetName; ///< name of the detector
+    uint64_t           fTimeStamp;  ///< timestamp, upper 32 bits
+                                    ///< for the seconds since 1970
+                                    ///< lower 32 for nanoseconds
     
 #ifndef __GCCXML__
   public:
     
-    AuxDetDigit(unsigned short channel,
+    AuxDetDigit(unsigned short     channel,
 		std::vector<short> adclist,
-		std::string name="UknownAuxDet");
+		std::string        name="UknownAuxDet",
+		uint64_t           timeStamp=UINT64_MAX);
     
     
     // Get Methods
@@ -41,6 +45,7 @@ namespace raw {
     short              ADC(size_t i) const;
     unsigned short     Channel()     const;
     std::string const& AuxDetName()  const;
+    uint64_t    const& TimeStamp()   const;
 
 #endif
   };
@@ -51,6 +56,7 @@ namespace raw {
 inline size_t             raw::AuxDetDigit::NADC()       const { return fADC.size(); }
 inline unsigned short     raw::AuxDetDigit::Channel()    const { return fChannel;    }
 inline std::string const& raw::AuxDetDigit::AuxDetName() const { return fAuxDetName; }
+inline uint64_t    const& raw::AuxDetDigit::TimeStamp()  const { return fTimeStamp;  }
 
 #endif
 
