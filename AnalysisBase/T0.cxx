8b85e47c3570bddc1236793a96a7c49a9f2a1647
Thomas Warburton <k.warburton@sheffield.ac.uk>
2015-10-01 15:29:10 -0500

    Adding TriggerConfidence to T0 object, and adding descriptions for each member

--- AnalysisBase/T0.cxx
+++ AnalysisBase/T0.cxx
@@ -24,10 +24,12 @@ namespace anab{
   T0::T0(double Time,
 	 unsigned int TriggerType,
 	 int TriggerBits,
+	 double TriggerConfidence,
 	 int ID)
     : fTime(Time)
     , fTriggerType(TriggerType)
     , fTriggerBits(TriggerBits)
+    , fTriggerConfidence(TriggerConfidence)
     , fID(ID)
   {
 
@@ -38,12 +40,12 @@ namespace anab{
   //
   std::ostream& operator<< (std::ostream & o, T0 const& a)
   {
-    o << "T0 with Time: "    << a.fTime
-      << "\n from Trigger type: "   << a.fTriggerType
-      << "\n with bits: "           << a.fTriggerBits
-      << "\n with ID: "             << a.fID
-      <<std::endl;
-    
+    o << "T0 with Time: "           << a.fTime              /// Time in ns
+      << "\n from Trigger type: "   << a.fTriggerType       /// Type of trigger used. 0 - Muon Counters, 1 - Photon Detectors, 2 - Monte Carlo Truth
+      << "\n with bits: "           << a.fTriggerBits       /// An identifier for the Muon track / Flash / MCParticle used in matching.
+      << "\n with confidence: "     << a.fTriggerConfidence /// Confidence with which this T0 is known.
+      << "\n with ID: "             << a.fID                /// Current size of T0 data product.
+      <<std::endl; 
 
     return o;
   }
