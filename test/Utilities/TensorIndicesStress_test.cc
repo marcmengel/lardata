9dfbb094ab448296f25a018d969528e30b74a48f
Gianluca Petrillo <petrillo@fnal.gov>
2016-06-29 13:23:21 -0500

    Added utility class to manage multi-dimension containers
    Added:
    - TensorIndices: index manager for arbitrary dimension indices
    - GridContainerIndices: based on TensorIndices, but with names connecting to a 2D and 3D space
    - GridContainers: containers based on std::vector using the indices above

---
+++ test/Utilities/TensorIndicesStress_test.cc
@@ -0,0 +1,107 @@
+/**
+ * @file   TensorIndicesStress_test.cc
+ * @brief  Stress test for TensorIndices class
+ * @author Gianluca Petrillo (petrillo@fnal.gov)
+ * @date   June 28, 2016
+ * @see    TensorIndices.h
+ * 
+ * This test performs repeated queries to a TensorIndices object of rank 5.
+ * Usage:
+ * ~~~~
+ * TensorIndicesStress_test [DimSize]
+ * ~~~~
+ * where `DimSize` is the dimension of each size (default: 16 and 32 for debug
+ * and non-debug compilation respectively).
+ * This test is not extremely representative of a real use case in that the loop
+ * content is small and can likely kept in the processor cache, which is not
+ * often the case in real scenarios.
+ * 
+ */
+
+// LArSoft libraries
+#include "lardata/Utilities/TensorIndices.h"
+
+// C/C++ standard libraries
+#include <stdexcept> // std::out_of_range
+#include <array>
+#include <chrono>
+#include <sstream>
+#include <iostream>
+
+
+//------------------------------------------------------------------------------
+//--- Test code
+//---
+
+int main(int argc, char** argv) {
+
+  // debug being much slower, by default we ask for less cycles
+#ifdef NDEBUG
+  unsigned int dimSize = 80; // default value
+#else // !NDEBUG
+  unsigned int dimSize = 32; // default value
+#endif // ?NDEBUG
+  
+  //
+  // command line argument parsing
+  //
+  if (argc > 1) {
+    std::istringstream sstr(argv[1]);
+    sstr >> dimSize;
+    if (!sstr || (dimSize <= 0)) {
+      std::cerr << "Invalid dimension size: '" << argv[1] << "'." << std::endl;
+      return 1;
+    }
+  }
+  
+  
+  //
+  // set up
+  //
+  auto indices
+    = util::makeTensorIndices(dimSize, dimSize, dimSize, dimSize, dimSize);
+  
+  std::cout << "Running through " << indices.dim<0>()
+    << "x" << indices.dim<1>()
+    << "x" << indices.dim<2>()
+    << "x" << indices.dim<3>()
+    << "x" << indices.dim<4>()
+    << " = " << indices.size() << " tensor elements"
+    << std::endl;
+  
+  //
+  // run
+  //
+  auto startTime = std::chrono::high_resolution_clock::now();
+  using index_t = decltype(indices)::Index_t;
+  std::array<index_t, indices.rank()> i;
+  decltype(indices)::DimSize_t count = 0;
+  for (i[0] = 0; i[0] < (index_t) indices.dim<0>(); ++(i[0])) {
+    for (i[1] = 0; i[1] < (index_t) indices.dim<1>(); ++(i[1])) {
+      for (i[2] = 0; i[2] < (index_t) indices.dim<2>(); ++(i[2])) {
+        for (i[3] = 0; i[3] < (index_t) indices.dim<3>(); ++(i[3])) {
+          for (i[4] = 0; i[4] < (index_t) indices.dim<4>(); ++(i[4])) {
+            
+            auto linIndex = indices(i[0], i[1], i[2], i[3], i[4]);
+            if (count != linIndex) {
+              std::cerr << "Error: ["
+                << i[0] << "][" << i[1] << "][" << i[2] << "]["
+                << i[3] << "][" << i[4]
+                << "] => " << linIndex << " (expected: " << count << ")"
+                << std::endl;
+              return 1;
+            }
+            ++count;
+          } // loop 4
+        } // loop 3
+      } // loop 2
+    } // loop 1
+  } // loop 0
+  auto stopTime = std::chrono::high_resolution_clock::now();
+  
+  std::chrono::duration<double> elapsed = stopTime - startTime;
+  std::cout << "Iterating through all " << count << " indices took "
+    << (elapsed.count() * 1000.) << " milliseconds." << std::endl;
+  
+  return 0;
+} // main()
---
+++ test/Utilities/TensorIndices_test.cc
@@ -0,0 +1,310 @@
+/**
+ * @file   TensorIndices_test.cc
+ * @brief  Test for TensorIndices class
+ * @author Gianluca Petrillo (petrillo@fnal.gov)
+ * @date   June 28, 2016
+ * @see    TensorIndices.h
+ * 
+ * This test instantiates some TensorIndices and verifies the reaction to some
+ * hard-coded settings and queries.
+ * 
+ * The test is run with no arguments.
+ * 
+ * Tests are run:
+ * 
+ * * `VectorTest`: one-dimension tensor test
+ * * `MatrixTest`: two-dimension tensor test
+ * * `TensorRank3Test`: test rank 3 tensor
+ * 
+ * See the documentation of the three functions for more information.
+ * 
+ */
+
+// LArSoft libraries
+#include "lardata/Utilities/TensorIndices.h"
+
+// Boost libraries
+#define BOOST_TEST_MODULE ( PointIsolationAlg_test )
+#include <boost/test/auto_unit_test.hpp> // BOOST_AUTO_TEST_CASE()
+#include <boost/test/test_tools.hpp> // BOOST_CHECK(), BOOST_CHECK_EQUAL()
+
+// C/C++ standard libraries
+#include <stdexcept> // std::out_of_range
+
+
+//------------------------------------------------------------------------------
+//--- Test code
+//---
+/**
+ * @brief Test for a rank 1 tensor (vector)
+ */
+void VectorTest() {
+  
+  // indices for a vector of size 4
+  util::TensorIndices<1> const indices{4};
+  
+  //
+  // reflection
+  //
+  BOOST_CHECK_EQUAL(indices.dim<0>(), 4U);
+  
+  BOOST_CHECK_EQUAL(indices.size<0>(), 4U);
+  
+  BOOST_CHECK_EQUAL(indices.size(), 4U);
+  
+  //
+  // indexing
+  //
+  std::array<char, 1> ii { 1 }; // char is for test; it should be size_t
+/* TODO
+  BOOST_CHECK_EQUAL(indices[0], 0U);
+  BOOST_CHECK_EQUAL(indices[1], 1U);
+  BOOST_CHECK_EQUAL(indices[2], 2U);
+  BOOST_CHECK_EQUAL(indices[3], 3U);
+  BOOST_CHECK_EQUAL(indices[4], 4U); // no bound check
+*/
+  BOOST_CHECK_EQUAL(indices(0), 0U);
+  BOOST_CHECK_EQUAL(indices(1), 1U);
+  BOOST_CHECK_EQUAL(indices(ii.begin()), 1U);
+  BOOST_CHECK_EQUAL(indices(2), 2U);
+  BOOST_CHECK_EQUAL(indices(3), 3U);
+  BOOST_CHECK_NO_THROW(indices(4)); // no bound check
+  
+  BOOST_CHECK_EQUAL(indices.at(0), 0U);
+  BOOST_CHECK_EQUAL(indices.at(1), 1U);
+  BOOST_CHECK_EQUAL(indices.at(ii.begin()), 1U);
+  BOOST_CHECK_EQUAL(indices.at(2), 2U);
+  BOOST_CHECK_EQUAL(indices.at(3), 3U);
+  BOOST_CHECK_THROW(indices.at(4), std::out_of_range);
+  
+  BOOST_CHECK(indices.has(0));
+  BOOST_CHECK(indices.has(1));
+  BOOST_CHECK(indices.has(ii.begin()));
+  BOOST_CHECK(indices.has(2));
+  BOOST_CHECK(indices.has(3));
+  BOOST_CHECK(!indices.has(4));
+  
+  BOOST_CHECK( indices.hasIndex<0>(0));
+  BOOST_CHECK( indices.hasIndex<0>(1));
+  BOOST_CHECK( indices.hasIndex<0>(2));
+  BOOST_CHECK( indices.hasIndex<0>(3));
+  BOOST_CHECK(!indices.hasIndex<0>(4));
+  BOOST_CHECK(!indices.hasIndex<0>(5));
+  
+  BOOST_CHECK(indices.hasLinIndex(0U));
+  BOOST_CHECK(indices.hasLinIndex(indices.size() - 1));
+  BOOST_CHECK(!indices.hasLinIndex(indices.size()));
+  
+  BOOST_CHECK_EQUAL(indices.size(), 4U);
+  
+  // check that the function also works
+  std::array<int, 1> ia { 4 };
+  util::TensorIndices<1> indicesAgain(ia.begin());
+  util::TensorIndices<1> indicesOther = util::makeTensorIndices(3);
+  util::TensorIndices<2> indicesRank  = util::makeTensorIndices(4, 3);
+  
+  // check the comparison operators
+  BOOST_CHECK(indicesAgain == indices);
+  BOOST_CHECK(!(indicesAgain != indices));
+  BOOST_CHECK(indicesOther != indices);
+  BOOST_CHECK(!(indicesOther == indices));
+  BOOST_CHECK(indicesRank != indices);
+  BOOST_CHECK(!(indicesRank == indices));
+  
+  
+} // VectorTest()
+
+
+/**
+ * @brief Test for a rank 2 tensor (matrix)
+ */
+void MatrixTest() {
+  
+  // indices for a matrix 4 x 3
+  util::TensorIndices<2> const indices{4, 3};
+  
+  //
+  // reflection
+  //
+  BOOST_CHECK_EQUAL(indices.dim<0>(), 4U);
+  BOOST_CHECK_EQUAL(indices.dim<1>(), 3U);
+  
+  BOOST_CHECK_EQUAL(indices.size<0>(), 4U * 3U);
+  BOOST_CHECK_EQUAL(indices.size<1>(), 3U);
+  
+  BOOST_CHECK_EQUAL(indices.size(), 4U * 3U);
+  
+  BOOST_CHECK_EQUAL(indices.minor().rank(), 1U);
+  BOOST_CHECK_EQUAL(indices.minor().size(), 3U);
+  
+  //
+  // indexing
+  //
+  std::array<char, 2> ii { 1, 2 }; // char is for test; it should be size_t
+/* TODO
+  BOOST_CHECK_EQUAL(indices[0], 0U);
+  BOOST_CHECK_EQUAL(indices[1], 1U);
+  BOOST_CHECK_EQUAL(indices[2], 2U);
+  BOOST_CHECK_EQUAL(indices[3], 3U);
+  BOOST_CHECK_EQUAL(indices[4], 4U); // no bound check
+*/
+  BOOST_CHECK_EQUAL(indices(0, 0), 0U);
+  BOOST_CHECK_EQUAL(indices(1, 2), 5U);
+  BOOST_CHECK_EQUAL(indices(ii.begin()), 5U);
+  BOOST_CHECK_NO_THROW(indices(1, 3));
+  BOOST_CHECK_NO_THROW(indices(4, 2));
+  BOOST_CHECK_NO_THROW(indices(7, 6));
+  
+  BOOST_CHECK_EQUAL(indices.at(0, 0), 0U);
+  BOOST_CHECK_EQUAL(indices.at(1, 2), 5U);
+  BOOST_CHECK_EQUAL(indices.at(ii.begin()), 5U);
+  BOOST_CHECK_THROW(indices.at(1, 3), std::out_of_range);
+  BOOST_CHECK_THROW(indices.at(4, 2), std::out_of_range);
+  BOOST_CHECK_THROW(indices.at(7, 6), std::out_of_range);
+  
+  BOOST_CHECK(indices.has(0, 0));
+  BOOST_CHECK(indices.has(ii.begin()));
+  BOOST_CHECK(indices.has(2, 2));
+  BOOST_CHECK(!indices.has(1, 3));
+  BOOST_CHECK(!indices.has(4, 2));
+  BOOST_CHECK(!indices.has(7, 6));
+  
+  BOOST_CHECK( indices.hasIndex<0>(0));
+  BOOST_CHECK( indices.hasIndex<0>(3));
+  BOOST_CHECK(!indices.hasIndex<0>(4));
+  BOOST_CHECK( indices.hasIndex<1>(0));
+  BOOST_CHECK( indices.hasIndex<1>(2));
+  BOOST_CHECK(!indices.hasIndex<1>(3));
+  
+  BOOST_CHECK(indices.hasLinIndex(0U));
+  BOOST_CHECK(indices.hasLinIndex(indices.size() - 1));
+  BOOST_CHECK(!indices.hasLinIndex(indices.size()));
+  
+  //
+  // comparisons
+  //
+  // check that the function and other constructors also works
+  std::array<int, 2> ia { 4, 3 };
+  util::TensorIndices<2> indicesAgain(ia.begin());
+  util::TensorIndices<2> indicesOther = util::makeTensorIndices(4, 4);
+  util::TensorIndices<3> indicesRank  = util::makeTensorIndices(5, 4, 3);
+  
+  // check the comparison operators
+  BOOST_CHECK(indicesAgain == indices);
+  BOOST_CHECK(!(indicesAgain != indices));
+  BOOST_CHECK(indicesOther != indices);
+  BOOST_CHECK(!(indicesOther == indices));
+  BOOST_CHECK(indicesRank != indices);
+  BOOST_CHECK(!(indicesRank == indices));
+  
+} // MatrixTest()
+
+
+/**
+ * @brief Test for a rank 3 tensor
+ */
+void TensorRank3Test() {
+  
+  // indices for a matrix 2 x 3 x 4
+  util::TensorIndices<3> const indices{ 2, 3, 4 };
+  
+  //
+  // reflection
+  //
+  BOOST_CHECK_EQUAL(indices.dim<0>(), 2U);
+  BOOST_CHECK_EQUAL(indices.dim<1>(), 3U);
+  BOOST_CHECK_EQUAL(indices.dim<2>(), 4U);
+  
+  BOOST_CHECK_EQUAL(indices.size<0>(), 2U * 3U * 4U);
+  BOOST_CHECK_EQUAL(indices.size<1>(), 3U * 4U);
+  BOOST_CHECK_EQUAL(indices.size<2>(), 4U);
+  
+  BOOST_CHECK_EQUAL(indices.size(), 2U * 3U * 4U);
+  
+  BOOST_CHECK_EQUAL(indices.minor().rank(), 2U);
+  BOOST_CHECK_EQUAL(indices.minor().size(), 3U * 4U);
+  
+  //
+  // indexing
+  //
+  std::array<char, 3> ii { 1, 2, 3 }; // char is for test; it should be size_t
+/* TODO
+  BOOST_CHECK_EQUAL(indices[0], 0U);
+  BOOST_CHECK_EQUAL(indices[1], 1U);
+  BOOST_CHECK_EQUAL(indices[2], 2U);
+  BOOST_CHECK_EQUAL(indices[3], 3U);
+  BOOST_CHECK_EQUAL(indices[4], 4U); // no bound check
+*/
+  BOOST_CHECK_EQUAL(indices(0, 0, 0), 0U);
+  BOOST_CHECK_EQUAL(indices(1, 2, 3), 23U);
+  BOOST_CHECK_EQUAL(indices(ii.begin()), 23U);
+  BOOST_CHECK_NO_THROW(indices(1, 3, 1));
+  BOOST_CHECK_NO_THROW(indices(4, 2, 1));
+  BOOST_CHECK_NO_THROW(indices(1, 2, 6));
+  BOOST_CHECK_NO_THROW(indices(7, 6, 6));
+  
+  BOOST_CHECK_EQUAL(indices.at(0, 0, 0), 0U);
+  BOOST_CHECK_EQUAL(indices.at(1, 2, 3), 23U);
+  BOOST_CHECK_EQUAL(indices.at(ii.begin()), 23U);
+  BOOST_CHECK_THROW(indices.at(1, 3, 1), std::out_of_range);
+  BOOST_CHECK_THROW(indices.at(4, 2, 1), std::out_of_range);
+  BOOST_CHECK_THROW(indices.at(1, 2, 6), std::out_of_range);
+  BOOST_CHECK_THROW(indices.at(7, 6, 6), std::out_of_range);
+  
+  BOOST_CHECK(indices.has(0, 0, 0));
+  BOOST_CHECK(indices.has(1, 2, 3));
+  BOOST_CHECK(indices.has(ii.begin()));
+  BOOST_CHECK(!indices.has(1, 3, 1));
+  BOOST_CHECK(!indices.has(4, 2, 1));
+  BOOST_CHECK(!indices.has(1, 2, 6));
+  BOOST_CHECK(!indices.has(7, 6, 6));
+  
+  BOOST_CHECK( indices.hasIndex<0>(0));
+  BOOST_CHECK( indices.hasIndex<0>(1));
+  BOOST_CHECK(!indices.hasIndex<0>(2));
+  BOOST_CHECK( indices.hasIndex<1>(0));
+  BOOST_CHECK( indices.hasIndex<1>(2));
+  BOOST_CHECK(!indices.hasIndex<1>(3));
+  BOOST_CHECK( indices.hasIndex<2>(0));
+  BOOST_CHECK( indices.hasIndex<2>(3));
+  BOOST_CHECK(!indices.hasIndex<2>(4));
+  
+  BOOST_CHECK(indices.hasLinIndex(0U));
+  BOOST_CHECK(indices.hasLinIndex(indices.size() - 1));
+  BOOST_CHECK(!indices.hasLinIndex(indices.size()));
+  
+  //
+  // comparisons
+  //
+  // check that the function also works
+  std::array<int, 3> ia { 2, 3, 4 };
+  util::TensorIndices<3> indicesAgain(ia.begin());
+  util::TensorIndices<3> indicesOther = util::makeTensorIndices(2, 3, 5);
+  util::TensorIndices<2> indicesRank  = util::makeTensorIndices(2, 3);
+  
+  // check the comparison operators
+  BOOST_CHECK(indicesAgain == indices);
+  BOOST_CHECK(!(indicesAgain != indices));
+  BOOST_CHECK(indicesOther != indices);
+  BOOST_CHECK(!(indicesOther == indices));
+  BOOST_CHECK(indicesRank != indices);
+  BOOST_CHECK(!(indicesRank == indices));
+  
+} // TensorRank3Test()
+
+
+//------------------------------------------------------------------------------
+//--- tests
+//
+BOOST_AUTO_TEST_CASE(VectorTestCase) {
+  VectorTest();
+} // VectorTestCase
+
+BOOST_AUTO_TEST_CASE(MatrixTestCase) {
+  MatrixTest();
+} // MatrixTestCase
+
+BOOST_AUTO_TEST_CASE(TensorRank3TestCase) {
+  TensorRank3Test();
+} // TensorRank3TestCase
+
