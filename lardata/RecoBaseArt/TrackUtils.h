9139f8efb4835f3c40edf782238701266f720574
Gianluca Petrillo <petrillo@fnal.gov>
2016-08-04 17:34:45 -0500

    Replaced nsmaepace lar::utils with lar::util (issue #12367)

--- lardata/RecoBaseArt/TrackUtils.h
+++ lardata/RecoBaseArt/TrackUtils.h
@@ -6,7 +6,7 @@
  * @see    TrackUtils.cxx
  * 
  * 
- * lar::utils::TrackProjectedLength() and lar::utils::TrackPitchInView() have
+ * lar::util::TrackProjectedLength() and lar::util::TrackPitchInView() have
  * been factored out from recob::Track, from recob::Track::ProjectedLength() and
  * recob::Track::PitchInView() respectively.
  */
@@ -20,7 +20,7 @@
 
 
 namespace lar {
-   namespace utils {
+   namespace util {
       
       /**
        * @brief Returns the length of the projection of a track on a view
@@ -52,7 +52,24 @@ namespace lar {
         (recob::Track const& track, geo::View_t view, size_t trajectory_point = 0);
       
       
-   } // namespace utils
+   } // namespace util
+
+
+   /// Old, deprecated namespace; use `lar::util` instead.
+   namespace utils {
+     
+      [[deprecated("use lar::util namespace instead")]]
+      inline auto TrackProjectedLength(recob::Track const& track, geo::View_t view)
+        { return ::lar::util::TrackProjectedLength(track, view); }
+
+      [[deprecated("use lar::util namespace instead")]]
+      inline auto TrackPitchInView
+        (recob::Track const& track, geo::View_t view, size_t trajectory_point = 0)
+        { return ::lar::util::TrackPitchInView(track, view, trajectory_point); }
+
+   }
+  
+  
 } // namespace lar
 
 
