165ecc00502a9f1c56545617562d10d41b932e3b
Saba Sehirsh <ssehrish@fnal.gov>
2016-11-23 22:52:18 -0600

    added a constructor for making Ptrs to products created in other modules

--- lardata/Utilities/PtrMaker.h
+++ lardata/Utilities/PtrMaker.h
@@ -6,11 +6,14 @@
  */
 
 #include "art/Framework/Core/EDProducer.h"
-#include "art/Framework/Core/FindManyP.h"
+//#include "art/Framework/Core/FindManyP.h"
+#include "canvas/Persistency/Common/FindManyP.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "cetlib/exception.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
+
+#include <iostream>
 
 namespace lar {
    // to create art::Ptrs in to a particular collection in an event
@@ -19,7 +22,10 @@ namespace lar {
    public:
       //Creates a PtrMaker that creates Ptrs in to a collection of type C created by the module of type MODULETYPE, where the collection has instance name "instance"
       template <class MODULETYPE, class C = std::vector<T>>
-      PtrMaker(art::Event const& evt, MODULETYPE const& module, std::string const& instance = std::string());
+      PtrMaker(art::Event const & evt, MODULETYPE const& module, std::string const& instance = std::string());
+      
+      //use this constructor when making Ptrs to products created in other modules
+      PtrMaker(art::Event const & evt, const art::ProductID & prodId, std::string const& instance = std::string());
       
       //Creates a Ptr to an object in the slot indicated by "index"
       art::Ptr<T> operator()(std::size_t index) const;
@@ -31,10 +37,16 @@ namespace lar {
    
    template <class T>
    template <class MODULETYPE, class C>
-   PtrMaker<T>::PtrMaker(art::Event const& evt, MODULETYPE const& module, std::string const & instance)
+   PtrMaker<T>::PtrMaker(art::Event const & evt, MODULETYPE const& module, std::string const & instance)
    : prodId(module.template getProductID<C>(evt, instance))
    , prodGetter(evt.productGetter(prodId))
-   {   }
+   {  }
+   
+   template <class T>
+   PtrMaker<T>::PtrMaker(art::Event const & evt, const art::ProductID & pid, std::string const & instance)
+   : prodId(pid)
+   , prodGetter(evt.productGetter(pid))
+   {  }
    
    template <class T>
    art::Ptr<T> PtrMaker<T>::operator()(size_t index) const
