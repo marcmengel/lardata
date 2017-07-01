2f7b622e66714498c10e62581c78b53eacdad2c2
Gianluca Petrillo <petrillo@fnal.gov>
2017-06-30 23:01:06 -0500

    First implementation of lar::FindAllP() to navigate chains of associations.
    Poorly tested.

---
+++ test/Utilities/testAssnsChainUtils/AssnsChainClusterMaker_module.cc
@@ -0,0 +1,232 @@
+/**
+ * @file   AssnsChainClusterMaker_module.cc
+ * @brief  Test producer creating a few dummy clusters associated with hits.
+ * @author Gianluca Petrillo (petrillo@fnal.gov)
+ * @date   June 23, 2017
+ */
+
+// LArSoft libraries
+#include "lardata/Utilities/PtrMaker.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RecoBase/Cluster.h"
+#include "larcoreobj/SimpleTypesAndConstants/geo_types.h" // geo namespace
+#include "larcoreobj/SimpleTypesAndConstants/RawTypes.h" // raw namespace
+
+// framework libraries
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/Assns.h"
+#include "canvas/Utilities/InputTag.h"
+
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Sequence.h"
+#include "fhiclcpp/types/Name.h"
+#include "fhiclcpp/types/Comment.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+// C/C++ standard libraries
+#include <algorithm> // std::accumulate()
+#include <vector>
+#include <utility> // std::move()
+#include <memory> // std::make_unique()
+#include <cmath> // std::sqrt()
+
+
+namespace lar {
+  namespace test {
+    
+    // -------------------------------------------------------------------------
+    /**
+    * @brief Creates some dummy clusters and associations to hits.
+    * 
+    * Configuration parameters
+    * =========================
+    * 
+    * * *hits* (list of input tags): collections of the hits to be clustered
+    * * *hitsPerCluster* (unsigned integer, default: 100): number of hits 
+    *   associated with each cluster
+    * 
+    */
+    class AssnsChainClusterMaker: public art::EDProducer {
+        public:
+      
+      struct Config {
+        using Name = fhicl::Name;
+        using Comment = fhicl::Comment;
+        
+        fhicl::Sequence<art::InputTag> hits{
+          Name("hits"),
+          Comment("collections of hits to be clustered")
+          };
+        
+        fhicl::Atom<unsigned int> hitsPerCluster{
+          Name("hitsPerCluster"),
+          Comment("number of hits associated with each cluster"),
+          100
+          };
+        
+      }; // struct Config
+      
+      using Parameters_t = art::EDProducer::Table<Config>;
+      
+      explicit AssnsChainClusterMaker(Parameters_t const& config)
+        : hitTags(config().hits())
+        , nHitsPerCluster(config().hitsPerCluster())
+        {
+          produces<std::vector<recob::Cluster>>();
+          produces<art::Assns<recob::Hit, recob::Cluster>>();
+        }
+
+      virtual void produce(art::Event& event) override;
+      
+        private:
+      std::vector<art::InputTag> hitTags; ///< List of hit tags for clustering.
+      unsigned int nHitsPerCluster; ///< Maximum number of hits per cluster.
+      
+      /// Returns a list of hits to be clustered.
+      std::vector<art::Ptr<recob::Hit>> collectHits
+        (art::Event const& event) const;
+      
+    };  // AssnsChainClusterMaker
+
+    // -------------------------------------------------------------------------
+    
+    
+  } // namespace test
+} // namespace lar
+
+
+// -----------------------------------------------------------------------------
+namespace {
+  
+  template <typename T>
+  inline T sqr(T v) { return v*v; }
+  
+} // local namespace
+
+// -----------------------------------------------------------------------------
+void lar::test::AssnsChainClusterMaker::produce(art::Event& event) {
+  
+  //
+  // prepare input: merge all hits in a single collection
+  //
+  std::vector<art::Ptr<recob::Hit>> hits = collectHits(event);
+  
+  //
+  // prepare output
+  //
+  auto clusters = std::make_unique<std::vector<recob::Cluster>>();
+  auto hitClusterAssns
+    = std::make_unique<art::Assns<recob::Hit, recob::Cluster>>();
+  
+  //
+  // create the clusters
+  //
+  unsigned int nClusters = hits.size() / nHitsPerCluster;
+  if (nClusters * nHitsPerCluster < hits.size()) ++nClusters;
+  
+  lar::PtrMaker<recob::Cluster> ptrMaker(event, *this);
+  
+  for (unsigned int i = 0; i < nClusters; ++i) {
+    
+    //
+    // assign hits to cluster
+    //
+    std::vector<art::Ptr<recob::Hit>> clusterHits;
+    std::size_t iHit = i;
+    while (iHit < hits.size()) {
+      clusterHits.push_back(hits[iHit]);
+      iHit += nClusters;
+    } // while
+    
+    //
+    // generate the cluster
+    //
+    clusters->emplace_back(
+      float(clusterHits.front()->WireID().Wire), // start_wire
+      1.0,                                       // sigma_start_wire
+      clusterHits.front()->PeakTime(),           // start_tick
+      clusterHits.front()->SigmaPeakTime(),      // sigma_start_tick
+      clusterHits.front()->Integral(),           // start_charge
+      0.0,                                       // start_angle
+      0.0,                                       // start_opening
+      float(clusterHits.back()->WireID().Wire),  // end_wire
+      1.0,                                       // sigma_end_wire
+      clusterHits.back()->PeakTime(),            // end_tick
+      clusterHits.back()->SigmaPeakTime(),       // sigma_end_tick
+      clusterHits.back()->Integral(),            // end_charge
+      0.0,                                       // end_angle
+      0.0,                                       // end_opening
+      std::accumulate(clusterHits.cbegin(), clusterHits.cend(), 0.0,
+        [](float sum, art::Ptr<recob::Hit> const& hit)
+          { return sum + hit->Integral(); }
+        ),                                       // integral
+      std::sqrt(std::accumulate(clusterHits.cbegin(), clusterHits.cend(), 0.0,
+        [](float sum, art::Ptr<recob::Hit> const& hit)
+          { return sum + sqr(hit->SigmaIntegral()); }
+        )),                                      // integral_stddev
+      std::accumulate(clusterHits.cbegin(), clusterHits.cend(), 0.0,
+        [](float sum, art::Ptr<recob::Hit> const& hit)
+          { return sum + hit->SummedADC(); }
+        ),                                       // summedADC
+      std::sqrt(std::accumulate(clusterHits.cbegin(), clusterHits.cend(), 0.0,
+        [](float sum, art::Ptr<recob::Hit> const& hit)
+          { return sum + hit->SummedADC(); }
+        )),                                      // summedADC_stddev
+      clusterHits.size(),                        // n_hits
+      0.0,                                       // multiple_hit_density
+      2.0,                                       // width
+      recob::Cluster::ID_t(i + 1),               // ID
+      clusterHits.front()->View(),               // view
+      clusterHits.front()->WireID().asPlaneID(), // plane
+      recob::Cluster::Sentry
+      );
+    
+    //
+    // generate associations
+    //
+    auto const clusterPtr = ptrMaker(i); // art pointer to the new cluster
+    for (art::Ptr<recob::Hit> const& hit: clusterHits)
+      hitClusterAssns->addSingle(hit, clusterPtr);
+    
+  } // for
+  
+  mf::LogInfo("AssnsChainClusterMaker")
+    << "Created " << clusters->size() << " clusters with about "
+    << nHitsPerCluster << " hits each from " << hits.size() << " hits and "
+    << hitClusterAssns->size() << " associations from "
+    << hitTags.size() << " collections";
+  
+  event.put(std::move(clusters));
+  event.put(std::move(hitClusterAssns));
+  
+} // lar::test::AssnsChainClusterMaker::produce()
+
+
+// -----------------------------------------------------------------------------
+std::vector<art::Ptr<recob::Hit>> lar::test::AssnsChainClusterMaker::collectHits
+  (art::Event const& event) const
+{
+  
+  std::vector<art::Ptr<recob::Hit>> allHits;
+  
+  for (auto const& tag: hitTags) {
+    auto hits = event.getValidHandle<std::vector<recob::Hit>>(tag);
+    
+    std::size_t const nHits = hits->size();
+    for (std::size_t i = 0; i < nHits; ++i)
+      allHits.emplace_back(hits, i);
+    
+  } // for
+  
+  return allHits;
+} // lar::test::AssnsChainClusterMaker::collectHits()
+
+
+// -----------------------------------------------------------------------------
+DEFINE_ART_MODULE(lar::test::AssnsChainClusterMaker)
+
+// -----------------------------------------------------------------------------
---
+++ test/Utilities/testAssnsChainUtils/AssnsChainPFParticleMaker_module.cc
@@ -0,0 +1,219 @@
+/**
+ * @file   AssnsChainPFParticleMaker_module.cc
+ * @brief  Test producer creating a few dummy PFParticles associated with
+ *         clusters.
+ * @author Gianluca Petrillo (petrillo@fnal.gov)
+ * @date   June 26, 2017
+ */
+
+// LArSoft libraries
+#include "lardata/Utilities/PtrMaker.h"
+#include "lardataobj/RecoBase/Cluster.h"
+#include "lardataobj/RecoBase/PFParticle.h"
+
+// framework libraries
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/Assns.h"
+#include "canvas/Utilities/InputTag.h"
+
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Sequence.h"
+#include "fhiclcpp/types/Name.h"
+#include "fhiclcpp/types/Comment.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+// C/C++ standard libraries
+#include <vector>
+#include <utility> // std::move()
+#include <memory> // std::make_unique()
+
+
+namespace lar {
+  namespace test {
+    
+    // -------------------------------------------------------------------------
+    /**
+    * @brief Creates some dummy PFParticles and associations to clusters.
+    * 
+    * Configuration parameters
+    * =========================
+    * 
+    * * *clusters* (list of input tags): collections of the clusters to be
+    *     used if PFParticles
+    * * *clustersPerPFO* (unsigned integer, default: 3): number of clusters
+    *   combined into each PFParticle
+    * 
+    */
+    class AssnsChainPFParticleMaker: public art::EDProducer {
+        public:
+      
+      struct Config {
+        using Name = fhicl::Name;
+        using Comment = fhicl::Comment;
+        
+        fhicl::Sequence<art::InputTag> clusters{
+          Name("clusters"),
+          Comment("collections of clusters to be combined")
+          };
+        
+        fhicl::Atom<unsigned int> clustersPerPFO{
+          Name("clustersPerPFO"),
+          Comment("number of clusters combined into each PFParticle"),
+          3
+          };
+        
+      }; // struct Config
+      
+      using Parameters_t = art::EDProducer::Table<Config>;
+      
+      explicit AssnsChainPFParticleMaker(Parameters_t const& config)
+        : clusterTags(config().clusters())
+        , nClustersPerPFO(config().clustersPerPFO())
+        {
+          produces<std::vector<recob::PFParticle>>();
+          produces<art::Assns<recob::Cluster, recob::PFParticle>>();
+        }
+
+      virtual void produce(art::Event& event) override;
+      
+        private:
+      std::vector<art::InputTag> clusterTags; ///< List of cluster tags.
+      unsigned int nClustersPerPFO; ///< Maximum number of clusters per PFO.
+      
+      /// Returns a list of clusters to be combined.
+      std::vector<art::Ptr<recob::Cluster>> collectClusters
+        (art::Event const& event) const;
+      
+    };  // AssnsChainPFParticleMaker
+
+    // -------------------------------------------------------------------------
+    
+    
+  } // namespace test
+} // namespace lar
+
+
+// -----------------------------------------------------------------------------
+void lar::test::AssnsChainPFParticleMaker::produce(art::Event& event) {
+  
+  //
+  // prepare input: merge all hits in a single collection
+  //
+  std::vector<art::Ptr<recob::Cluster>> clusters = collectClusters(event);
+  
+  //
+  // prepare output
+  //
+  auto PFOs = std::make_unique<std::vector<recob::PFParticle>>();
+  auto clusterPFOassns
+    = std::make_unique<art::Assns<recob::Cluster, recob::PFParticle>>();
+  
+  //
+  // create the PFParticles
+  //
+  unsigned int nPFOs = clusters.size() / nClustersPerPFO;
+  if (nPFOs * nClustersPerPFO < clusters.size()) ++nPFOs;
+  
+  lar::PtrMaker<recob::PFParticle> ptrMaker(event, *this);
+  
+  unsigned int nDaughtersPerParticle = 2;
+  unsigned int nParticlesInTier = 1;
+  std::size_t firstPFOinThisTier = 0;
+  std::size_t firstPFOinNextTier = firstPFOinThisTier + nParticlesInTier;
+  std::size_t nextDaughter = firstPFOinNextTier;
+  std::vector<std::size_t> parents
+    (nPFOs, recob::PFParticle::kPFParticlePrimary);
+  for (unsigned int i = 0; i < nPFOs; ++i) {
+    
+    // prepare for the next tier, if needed
+    if (i >= firstPFOinNextTier) {
+      firstPFOinThisTier = firstPFOinNextTier;
+      
+      nParticlesInTier *= nDaughtersPerParticle;
+      nDaughtersPerParticle += 1;
+      firstPFOinNextTier += nParticlesInTier;
+    } // if
+    
+    //
+    // assign clusters to PFO
+    //
+    std::vector<art::Ptr<recob::Cluster>> PFOclusters;
+    std::size_t iCluster = i;
+    while (iCluster < clusters.size()) {
+      PFOclusters.push_back(clusters[iCluster]);
+      iCluster += nPFOs;
+    } // while
+    
+    //
+    // generate the PFParticle
+    //
+    std::size_t const endDaughter
+      = std::min(nextDaughter + nDaughtersPerParticle, (std::size_t) nPFOs);
+    std::vector<std::size_t> daughters;
+    daughters.reserve(endDaughter - nextDaughter);
+    while (nextDaughter < endDaughter) {
+      parents[nextDaughter] = i;
+      daughters.push_back(nextDaughter);
+      ++nextDaughter;
+    } // while
+    
+    PFOs->emplace_back(
+      11,                  // pdgCode (11 = shower-like)
+      i,                   // self
+      parents[i],          // parent
+      std::move(daughters)
+      );
+    
+    //
+    // generate associations
+    //
+    auto const PFOptr = ptrMaker(i); // art pointer to the new PFParticle
+    for (art::Ptr<recob::Cluster> const& cluster: PFOclusters) {
+      mf::LogVerbatim("AssnsChainPFParticleMaker")
+        << "Associating cluster " << cluster << " with PFO " << PFOptr;
+      clusterPFOassns->addSingle(cluster, PFOptr);
+    }
+    
+  } // for
+  
+  mf::LogInfo("AssnsChainPFParticleMaker")
+    << "Created " << PFOs->size() << " particle flow objects with about "
+    << nClustersPerPFO << " clusters each from " << clusters.size()
+    << " clusters and " << clusterPFOassns->size() << " associations from "
+    << clusterTags.size() << " collections";
+  
+  event.put(std::move(PFOs));
+  event.put(std::move(clusterPFOassns));
+  
+} // lar::test::AssnsChainClusterMaker::produce()
+
+
+// -----------------------------------------------------------------------------
+std::vector<art::Ptr<recob::Cluster>>
+lar::test::AssnsChainPFParticleMaker::collectClusters
+  (art::Event const& event) const
+{
+  
+  std::vector<art::Ptr<recob::Cluster>> allClusters;
+  
+  for (auto const& tag: clusterTags) {
+    auto clusters = event.getValidHandle<std::vector<recob::Cluster>>(tag);
+    
+    std::size_t const nClusters = clusters->size();
+    for (std::size_t i = 0; i < nClusters; ++i)
+      allClusters.emplace_back(clusters, i);
+    
+  } // for
+  
+  return allClusters;
+} // lar::test::AssnsChainPFParticleMaker::collectClusters()
+
+
+// -----------------------------------------------------------------------------
+DEFINE_ART_MODULE(lar::test::AssnsChainPFParticleMaker)
+
+// -----------------------------------------------------------------------------
