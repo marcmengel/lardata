55d5a8920712b60975e65960b82f83a14ac1c149
Lynn Garren <garren@fnal.gov>
2019-10-02 11:05:19 -0500

    move MarqFitAlg here from larreco

--- lardata/Utilities/LArFFTW.h
+++ lardata/Utilities/LArFFTW.h
@@ -11,7 +11,7 @@
 
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "cetlib_except/coded_exception.h"
-#include "larreco/HitFinder/HitFinderTools/MarqFitAlg.h"
+#include "lardata/Utilities/MarqFitAlg.h"
 
 namespace util {
 
